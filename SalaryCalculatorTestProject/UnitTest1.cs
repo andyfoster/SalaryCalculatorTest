ba333fc993103eb0255e42b8f4c80a385edeb1af
Andy Foster <andrewfosteris@gmail.com>
2020-10-26 21:52:58 +1300

    Set up failing tests for annual salary calculator

--- SalaryCalculatorTestProject/UnitTest1.cs
+++ SalaryCalculatorTestProject/UnitTest1.cs
@@ -2,12 +2,28 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SalaryCalculatorTestProject
 {
+    /*
+         To get hourly, divide annual salary by 2000
+            $100,006.40 / 2000  = $48 hr
+
+         To get annual, multiply hourly by 2000
+            $48 * 2000 = $100,006.40
+     */
+
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AnnualSalaryTest()
         {
+            // Arrange
+            SalaryCalculator sc = new SalaryCalculator();
+
+            // Act
+            decimal annualSalary = sc.GetAnnualSalary(50);
+
+            // Assert
+            Assert.AreEqual(104000, annualSalary);
         }
     }
 }
